4e83b69324a699f68c231bcb6d8e728218280acf
Chris <saaepear@gmail.com>
2026-04-09 01:24:38 -0500

    feat: actualizar modelo User, migración y factory

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
+            $table->string('apellido');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password'); // Contraseña
             $table->rememberToken();
             $table->timestamps();
         });
